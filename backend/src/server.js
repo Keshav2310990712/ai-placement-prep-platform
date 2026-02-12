fe812509ad526a13c1686a0ea881c389284a4432
keshav <keshav0712.be23@chitkara.edu.in>
2026-02-12 10:17:23 +0530

    Phase 4: JWT Authentication implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,15 +2,23 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const morgan = require("morgan");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/auth.routes");
+
 
 dotenv.config();
 
 const app = express();
 
+connectDB();
+
 // Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+
+app.use("/api/auth", authRoutes);
+
 
 // Health route
 app.get("/api/health", (req, res) => {
